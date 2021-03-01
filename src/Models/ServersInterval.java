1c6dbcfe886b3ace4659c522deb6bde63284cd2b
Tom <tom.hoad03@gmail.com>
2021-03-01 16:06:35 +0000

    Interval classes

---
+++ src/Models/ServersInterval.java
@@ -0,0 +1,14 @@
+package Models;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class ServersInterval extends Interval {
+    private ArrayList<Server> servers;
+
+    public ServersInterval(LocalDateTime date, ArrayList<Server> servers) {
+        super(date);
+
+        this.servers = servers;
+    }
+}
