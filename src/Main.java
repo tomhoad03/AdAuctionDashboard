bc97252878f15f4225965540094ca3481c328902
Tom <tom.hoad03@gmail.com>
2021-03-01 10:43:55 +0000

    Changing the Date data type to LocalDateTime

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import Models.Campaign;
+import Models.ChartCalculator;
 import Models.MetricCalculator;
 
 public class Main {
     public static void main(String[] args) {
         Campaign campaign = new Campaign("src/Logs/impression_log.csv", "src/Logs/click_log.csv", "src/Logs/server_log.csv"); // string inputs temporary
 
-        MetricCalculator calculator1 = campaign.newCalculator();
-        MetricCalculator calculator2 = campaign.newCalculator();
+        MetricCalculator calculator1 = campaign.newMetricCalculator();
+        MetricCalculator calculator2 = campaign.newMetricCalculator();
 
         // first calculator for main metrics page
         // all metrics
@@ -16,14 +17,19 @@ public class Main {
         System.out.println(" ");
 
         // subsequent calculators for charts etc.
-        // metrics within a time frame, and different filtering for later
-        // calculator2.calculateMetrics(2, 250, "2015-01-01 12:01:21", "2015-01-01 13:51:59"); // not useful atm
-        // print(calculator2);
+        // metrics within a time frame, and different filtering for later, not useful atm
+        calculator2.calculateMetrics(2, 250, "2015-01-01 12:01:21", "2015-01-01 13:51:59");
+        print(calculator2);
 
-        // a new calculator should be created for each chart, graph or histogram we need
-        // the print functions are to be replaced with far more variety
-        // atm you can print all, or get each metric individually (OOP getters)
-        // atm we can only show them in a terminal but in future this will be changed to produce outputs that can be used in charts and histograms
+        /**
+        a new calculator should be created for each chart, graph or histogram we need
+        the print functions are to be replaced with far more variety
+        atm you can print all, or get each metric individually (OOP getters)
+        atm we can only show them in a terminal but in future this will be changed to produce outputs that can be used in charts and histograms
+        **/
+
+        ChartCalculator calculator3 = campaign.newChartCalculator();
+        calculator3.createDates();
     }
 
     // temporary function to display metrics in terminal
